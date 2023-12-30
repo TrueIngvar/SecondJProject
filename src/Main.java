03e3f5d994b7f48df56ca562c19fa83c3ce2b02b
TrueIngvar <igor171298@mail.ru>
2023-12-30 21:21:16 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 20_000;
+        int aMile = 20;
+        int allMiles = cost / aMile;
+        System.out.println(allMiles);
+    }
+}
